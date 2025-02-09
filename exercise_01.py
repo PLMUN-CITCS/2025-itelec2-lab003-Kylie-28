e38b8b0ed527a918d74b668f07b1e769b7649194
kylie-28 <bolivaralexkyle_bsit@plmun.edu.ph>
2025-02-09 20:41:55 +0800

    Update exercise_01.py

--- exercise_01.py
+++ exercise_01.py
@@ -2,6 +2,7 @@
 # ITELEC2
 # Laboratory #03 – Guided Coding Exercise:
 # Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
 count = 10
 Count = 15
 decimal_value = 3.14
@@ -13,11 +14,17 @@ result = None
 total_count = 20
 
 print("Integer (count):", count)
+
 print("Integer (Count):", Count)
+
 print("Integer (total_count):", total_count)
+
 print("Decimal:", decimal_value)
+
 print("Text:", message)
+
 print("Boolean:", is_active)
+
 print("None Value:", result)
 
 num1 = 5
